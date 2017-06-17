8783c796822d23ccf5c481a5b1834ca497abe409
zhiwenyan <zhiwenyan@icloud.com>
2017-06-17 23:07:04 +0800

    流体式标签

---
+++ app/src/main/java/zhiwenyan/cmccaifu/com/android2017/view/touchView/ViewTouchActivity.java
@@ -0,0 +1,31 @@
+package zhiwenyan.cmccaifu.com.android2017.view.touchView;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import zhiwenyan.cmccaifu.com.android2017.R;
+
+public class ViewTouchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_touch);
+        findViewById(R.id.tv).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.i("TAG", "onTouch: " + event.getAction());
+                return false;
+            }
+        });
+        findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("TAG", "onClick");
+            }
+        });
+    }
+}
