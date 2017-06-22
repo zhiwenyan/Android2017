068c99a7424d03ff3367a9c5b2fb2931420eba52
zhiwenyan <zhiwenyan@icloud.com>
2017-06-22 13:07:22 +0800

    卡片效果

--- app/src/main/java/zhiwenyan/cmccaifu/com/android2017/banner/cardViewPager/CardViewPagerActivity.java
+++ app/src/main/java/zhiwenyan/cmccaifu/com/android2017/banner/cardViewPager/CardViewPagerActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -24,7 +25,6 @@ public class CardViewPagerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_card_view_pager);
         mViewPager = (ViewPager) findViewById(R.id.viewPager);
         mViewPager.setPageMargin(getResources().getDimensionPixelSize(R.dimen.fab_margin));
-        mViewPager.setOffscreenPageLimit(3);
 
         CardViewPagerAdapter adapter = new CardViewPagerAdapter(getSupportFragmentManager(), 3f);
         mViewPager.setAdapter(adapter);
@@ -74,102 +74,25 @@ public class CardViewPagerActivity extends AppCompatActivity {
         }
     }
 
-    private class ShadowTransformer implements ViewPager.OnPageChangeListener, ViewPager.PageTransformer {
-
-        private ViewPager mViewPager;
-        private CardAdapter mAdapter;
-        private float mLastOffset;
-        private boolean mScalingEnabled;
+    private class ShadowTransformer implements ViewPager.PageTransformer {
         private final float MIN_SCALE = 0.8f;
 
 
         public ShadowTransformer(ViewPager viewPager, CardAdapter adapter) {
             mViewPager = viewPager;
-            viewPager.addOnPageChangeListener(this);
-            mAdapter = adapter;
         }
 
-        public void enableScaling(boolean enable) {
-            if (mScalingEnabled && !enable) {
-                // shrink main card
-                CardView currentCard = mAdapter.getCardViewAt(mViewPager.getCurrentItem());
-                if (currentCard != null) {
-                    currentCard.animate().scaleY(1);
-                    currentCard.animate().scaleX(1);
-                }
-            } else if (!mScalingEnabled && enable) {
-                // grow main card
-                CardView currentCard = mAdapter.getCardViewAt(mViewPager.getCurrentItem());
-                if (currentCard != null) {
-                    currentCard.animate().scaleY(1.1f);
-                    currentCard.animate().scaleX(1.1f);
-                }
-            }
-
-            mScalingEnabled = enable;
-        }
 
         @Override
         public void transformPage(View page, float position) {
+            Log.i("TAG", "transformPage: " + position);
             if (position >= -1 && position <= 1) {
                 page.setScaleY(1.0f - Math.abs(position) * (1 - MIN_SCALE));
             } else {
                 page.setScaleY(MIN_SCALE);
             }
 
-        }
-
-        @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//            int realCurrentPosition;
-//            int nextPosition;
-//            float baseElevation = mAdapter.getBaseElevation();
-//            float realOffset;
-//            boolean goingLeft = mLastOffset > positionOffset;
-//            if (goingLeft) {
-//                realCurrentPosition = position + 1;
-//                nextPosition = position;
-//                realOffset = 1 - positionOffset;
-//            } else {
-//                nextPosition = position + 1;
-//                realCurrentPosition = position;
-//                realOffset = positionOffset;
-//            }
-//            if (nextPosition > mAdapter.getCount() - 1
-//                    || realCurrentPosition > mAdapter.getCount() - 1) {
-//                return;
-//            }
-//            CardView currentCard = mAdapter.getCardViewAt(realCurrentPosition);
-//            if (currentCard != null) {
-//                if (mScalingEnabled) {
-//                    currentCard.setScaleX((float) (1 + 0.1 * (1 - realOffset)));
-//                    currentCard.setScaleY((float) (1 + 0.1 * (1 - realOffset)));
-//                }
-//                currentCard.setCardElevation((baseElevation + baseElevation
-//                        * (CardAdapter.MAX_ELEVATION_FACTOR - 1) * (1 - realOffset)));
-//            }
-//            CardView nextCard = mAdapter.getCardViewAt(nextPosition);
-//            if (nextCard != null) {
-//                if (mScalingEnabled) {
-//                    nextCard.setScaleX((float) (1 + 0.1 * (realOffset)));
-//                    nextCard.setScaleY((float) (1 + 0.1 * (realOffset)));
-//                }
-//                nextCard.setCardElevation((baseElevation + baseElevation
-//                        * (CardAdapter.MAX_ELEVATION_FACTOR - 1) * (realOffset)));
-//            }
-//            mLastOffset = positionOffset;
-
-        }
-
-        @Override
-        public void onPageSelected(int position) {
-
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int state) {
 
         }
     }
-
 }
